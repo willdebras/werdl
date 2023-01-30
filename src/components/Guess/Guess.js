fc4a1c8181ed4b0ea0bb6011750086e5ec3ffe2a
willdebras <wbonnell123@gmail.com>
2023-01-30 08:53:54 -0600

    code cleanup

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -2,13 +2,12 @@ import React from "react";
 import { range } from '../../utils';
 
 function Guess({guessValue}) {
-  console.log(guessValue)
   return <>
     <p className="guess">
       {range(0, 5).map(index => (
         <span className="cell" key={index}>{guessValue && guessValue[index]}</span>
       ))}
-  </p>
+    </p>
   </>
 }
 
