d5ed7c523cd1ece003493799d4fa8262601faa4b
willdebras <wbonnell123@gmail.com>
2023-01-30 08:50:16 -0600

    past guesses interface working minus game logic

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { range } from '../../utils';
 
-function Guess(guessValue) {
+function Guess({guessValue}) {
   console.log(guessValue)
   return <>
     <p className="guess">
-      {range(1, 6).map(index => (
-        <span className="cell" key={index}></span>
+      {range(0, 5).map(index => (
+        <span className="cell" key={index}>{guessValue && guessValue[index]}</span>
       ))}
   </p>
   </>
