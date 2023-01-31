8cf627891b0c096ea56c4da921b65454febabab4
willdebras <wbonnell123@gmail.com>
2023-01-30 22:02:44 -0600

    silly header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,8 @@ import React from 'react';
 function Header() {
   return (
     <header>
-      <h1>Word Game</h1>
+      <h1>werdl</h1>
+      <p>the only werd gaem wear yuo luk 4 a misspeled werd</p>
     </header>
   );
 }
